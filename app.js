eef115737404bb859b3d42471035c185327b05a7
uma shankar <umashankar.kousha@gmail.com>
2025-09-27 14:56:39 +0530

    auto quote generator logic improved

---
+++ app.js
@@ -0,0 +1,55 @@
+const codingQuotes = [
+  {
+    text: "Talk is cheap. Show me the code.",
+    author: "Linus Torvalds",
+  },
+  {
+    text: "Programs must be written for people to read, and only incidentally for machines to execute.",
+    author: "Harold Abelson",
+  },
+  {
+    text: "First, solve the problem. Then, write the code.",
+    author: "John Johnson",
+  },
+  {
+    text: "Code is like humor. When you have to explain it, it’s bad.",
+    author: "Cory House",
+  },
+  {
+    text: "Before software can be reusable, it first has to be usable.",
+    author: "Ralph Johnson",
+  },
+  {
+    text: "Simplicity is the soul of efficiency.",
+    author: "Austin Freeman",
+  },
+  {
+    text: "Experience is the name everyone gives to their mistakes.",
+    author: "Oscar Wilde",
+  },
+  {
+    text: "The best error message is the one that never shows up.",
+    author: "Thomas Fuchs",
+  },
+  {
+    text: "The function of good software is to make the complex appear simple.",
+    author: "Grady Booch",
+  },
+  {
+    text: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    author: "Martin Fowler",
+  },
+];
+
+let quoteEl = document.querySelector(".quote");
+let authorEl = document.querySelector(".author");
+
+let nextQuoteEl = document.querySelector(".next-btn");
+quoteEl.textContent = codingQuotes[0].text;
+authorEl.textContent = "-" + codingQuotes[0].author;
+
+nextQuoteEl.addEventListener("click", function () {
+  let randomNumber = Math.floor(Math.random() * codingQuotes.length);
+  quoteEl.textContent = codingQuotes[randomNumber].text;
+  authorEl.textContent = "-" + codingQuotes[randomNumber].author;
+});
